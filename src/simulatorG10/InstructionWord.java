ec770c71be63bfc378c7435cadfd583efd54d4db
harshagangari747 <harshagangari747@gmail.com>
2023-11-25 22:27:26 -0500

    Added IN,OUT implementation

--- src/simulatorG10/InstructionWord.java
+++ src/simulatorG10/InstructionWord.java
@@ -10,8 +10,9 @@ public class InstructionWord {
 	boolean indirectAddressing;
 	int address;
 
-	/* Class constructor to set the different fields of the instruction word
-	 * for the project 1 address values up to 5bits = 2^5 = 0-31 are supported
+	/*
+	 * Class constructor to set the different fields of the instruction word for the
+	 * project 1 address values up to 5bits = 2^5 = 0-31 are supported
 	 */
 	public InstructionWord(String opcode, String gpr, String index, String ia, String _address) throws Exception {
 		this.opCode = GetOpcode(opcode);
@@ -23,11 +24,15 @@ public class InstructionWord {
 	}
 
 	/*
-	 * Based on the first six bits, returns the appropriate opcode enum
-	 * For project 1 only LDR,STR,LDA,LDX,STX are supported
+	 * Based on the first six bits, returns the appropriate opcode enum For project
+	 * 1 only LDR,STR,LDA,LDX,STX are supported
 	 */
 	private OpCodes GetOpcode(String opcode) throws Exception {
 		switch (opcode) {
+		case "000000":
+		{
+			return opCode.HALT;
+		}
 		case "": {
 			return OpCodes.LOC;
 		}
@@ -46,86 +51,72 @@ public class InstructionWord {
 		case "100010": {
 			return OpCodes.STX;
 		}
-		case "001000":
-		{
+		case "001000": {
 			return OpCodes.JZ;
 		}
-		//Write all opcodes here
-		case "001001":
-		{
+		// Write all opcodes here
+		case "001001": {
 			return opCode.JNE;
 		}
-		case "001010":
-		{
+		case "001010": {
 			return opCode.JCC;
 		}
-		case "001011":
-		{
+		case "001011": {
 			return opCode.JMA;
 		}
-		case "001100":
-		{
+		case "001100": {
 			return opCode.JSR;
 		}
-		case "001101":
-		{
+		case "001101": {
 			return opCode.RFS;
 		}
-		case "001110":
-		{
+		case "001110": {
 			return opCode.SOB;
 		}
-		case "001111":
-		{
+		case "001111": {
 			return opCode.JGE;
 		}
-		case "000100":
-		{
+		case "000100": {
 			return opCode.AMR;
 		}
-		case "000101":
-		{
+		case "000101": {
 			return opCode.SMR;
 		}
-		case "000110":
-		{
+		case "000110": {
 			return opCode.AIR;
 		}
-		case "000111":
-		{
+		case "000111": {
 			return opCode.SIR;
 		}
-		case "010000":
-		{
+		case "010000": {
 			return opCode.MLT;
 		}
-		case "010001":
-		{
+		case "010001": {
 			return opCode.DVD;
 		}
-		case "010010":
-		{
+		case "010010": {
 			return opCode.TRR;
 		}
-		case "010100":
-		{
+		case "010100": {
 			return opCode.ORR;
 		}
-		case "010011":
-		{
+		case "010011": {
 			return opCode.AND;
 		}
-		case "010101":
-		{
+		case "010101": {
 			return opCode.NOT;
 		}
-		case "011001":
-		{
+		case "011001": {
 			return opCode.SRC;
 		}
-		case "011010":
-		{
+		case "011010": {
 			return opCode.RRC;
+		}
+		case "110001": {
+			return opCode.IN;
+		}
+		case "110010": {
+			return opCode.OUT;
 		}
 
 		default:
@@ -135,8 +126,8 @@ public class InstructionWord {
 
 	/*
 	 * Based on the [6,7] or [8,9] bits, returns the appropriate register enum value
-	 * isGPR is to identify if the caller is seeking to get enum for GPR registers or
-	 * Index registers
+	 * isGPR is to identify if the caller is seeking to get enum for GPR registers
+	 * or Index registers
 	 */
 	private Registers GetRegistersToSet(String register, boolean isGPR) {
 		switch (register) {
